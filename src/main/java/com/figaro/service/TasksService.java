99a417f931d3bdd60e0b6fd72c36293412cc62ed
Gonzalo <gonzalocasal@gmail.com>
2018-03-19 10:31:56 -0300

    mail sender

--- src/main/java/com/figaro/service/TasksService.java
+++ src/main/java/com/figaro/service/TasksService.java
@@ -2,6 +2,7 @@ package com.figaro.service;
 
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -15,10 +16,13 @@ import com.sendgrid.SendGrid;
 
 public class TasksService {
 	
-	 public ResponseEntity<String>  sendMail() throws IOException {
+	
+	final static Logger LOGGER = Logger.getLogger(TasksService.class);
+	
+	 public void  sendMail(String email) throws IOException {
 	    Email from = new Email("no-responder@figaro.com.ar");
 	    String subject = "FIGARO, empleados mail test";
-	    Email to = new Email("gonzalocasal@gmail.com");
+	    Email to = new Email(email);
 	    Content content = new Content("text/plain", "Hola Buen dia :) estos son tus turnos para hoy:");
 	    Mail mail = new Mail(from, subject, to, content);
 	    SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
@@ -31,8 +35,13 @@ public class TasksService {
 	    } catch (IOException ex) {
 	      throw ex;
 	    }
-	    return new ResponseEntity<>("OK", HttpStatus.OK);
 	  }
 	
-	
+	public void testJob() throws IOException {
+		LOGGER.info("CORRIENDO JOB");
+		sendMail("matimarena@gmail.com");
+		sendMail("ricardo.surribas@gmail.com");
+		sendMail("gonzalocasal@gmail.com");
+		
+	}
 }
