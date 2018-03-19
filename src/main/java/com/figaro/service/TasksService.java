76c205bec7b91bbbe09dbc0c3300ae181b3ba9fc
Gonzalo <gonzalocasal@gmail.com>
2018-03-19 15:26:42 -0300

    mail style

--- src/main/java/com/figaro/service/TasksService.java
+++ src/main/java/com/figaro/service/TasksService.java
@@ -1,6 +1,7 @@
 package com.figaro.service;
 
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,25 +27,7 @@ public class TasksService {
 	private EmpleadosService empleadosService;
 	
 	
-	private String emailTemplate = " <html>\r\n" + 
-			"    <head>\r\n" + 
-			"        <title>Figaro Mail</title>\r\n" + 
-			"    </head>\r\n" + 
-			"    <body>\r\n" + 
-			"        <h1 style=\"background-color: #292961;    box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 3px 0px, rgba(0, 0, 0, 0.14) 0px 1px 1px 0px, rgba(0, 0, 0, 0.12) 0px 2px 1px -1px; \">\r\n" + 
-			"        <img width=\"160\" height=\"80\" src=\"https://beta.figaro.com.ar/imgs/logo.png\">\r\n" + 
-			"        </h1>\r\n" + 
-			"        <table>\r\n" + 
-			"            <tr style=\"background-color: #292961; color: white\">\r\n" + 
-			"                <td>HORARIO</td>\r\n" + 
-			"                <td>CLIENTE</td>\r\n" + 
-			"                <td>TRABAJOS</td>\r\n" + 
-			"            </tr>\r\n" + 
-			"            %s\r\n" + 
-			"        </table>\r\n" + 
-			"    </body>\r\n" + 
-			"\r\n" + 
-			"</html>";
+	private String emailTemplate = "<html> <head> <title>Figaro Mail</title> </head> <body> <style type=\"text/css\"> body{font-family: Roboto-Regular,Helvetica,Arial,sans-serif; text-align: center; background-color: #f6f6f6; width:100%;}.header{background-color: #292961; color: white}table{display: inline-table; width: 600px; border-collapse: collapse;}td, th{padding-left: 8px;}</style> <table> <tr class=header > <td>Horario</td><td>Cliente</td><td>Trabajos</td></tr>%s </table> <footer> <img width=\"160\" height=\"80\" src=\"https://beta.figaro.com.ar/imgs/logo2.png\"> </footer> </body></html>";
 	
 	
 	 public void  sendMail(String email, String subject ,String message) {
@@ -71,13 +54,12 @@ public class TasksService {
 		LOGGER.info("CORRIENDO JOB");
 		List<TurnoDTO> turnosDelDia = turnosService.getTurnosDelDia(new Date());
 		List<Empleado> empleadosDisponibles = empleadosService.getEmpleadosDisponibles(new Date());
-		
 		for (Empleado e : empleadosDisponibles) {
 			List<TurnoDTO> turnosEmpleado = turnosDelDia.stream().filter(t -> t.getEmpleado().equals(e)).collect(Collectors.toList());
 			String turnos ="";
 			for(TurnoDTO t : turnosEmpleado) {
 				String turno = "<tr>%s</tr>";
-				String hora = "<td>fruta</td>";
+				String hora = "<td>"+generateHoraTurno(t) +"</td>";
 				String cliente = "<td>"+t.getCliente().getNombre() +" "+t.getCliente().getApellido()+"</td>"; 
 				String trabajos = "<td>"+t.getDescripcionTrabajos()+"</td>";
 				turno = String.format(turno, hora+cliente+trabajos);
@@ -87,11 +69,20 @@ public class TasksService {
 			String subject = String.format("Buen día %s :) estos son tus turnos para hoy:", e.getNombre());
 			sendMail(e.getEmail(), subject, email);
 		}
-		
-		
-		
 	}
 
+	private String generateHoraTurno(TurnoDTO t) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(t.getDesde());
+		String desde = (calendar.get(Calendar.HOUR_OF_DAY)) +":"+(calendar.get(Calendar.MINUTE)); 
+		calendar.setTime(t.getHasta());
+		String hasta = (calendar.get(Calendar.HOUR_OF_DAY)) +":"+(calendar.get(Calendar.MINUTE));
+		return desde+" - "+hasta;
+	}
+
+	
+	
+	
 	public TurnosService getTurnosService() {
 		return turnosService;
 	}
