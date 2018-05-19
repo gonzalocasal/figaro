60cae7a07deeeae95a87f8b460c7a4128657420d
gonzalo <gonzalocasal@gmail.com>
2018-05-19 19:32:19 -0300

    agregar cantidad de notificaciones en el titulo

--- src/main/webapp/js/notificacionesController.js
+++ src/main/webapp/js/notificacionesController.js
@@ -32,7 +32,8 @@ app.controller('notificacionesController', function ($scope, $http) {
 	$scope.cantidad = function(){
         $http.get("/rest/notificaciones/cantidad").then(function (response) {
             $scope.cantidadNotificaciones = response.data;
-            document.title = "("+$scope.cantidadNotificaciones+") "+document.title ;
+            if($scope.cantidadNotificaciones > 0)
+                document.title = "("+$scope.cantidadNotificaciones+") "+document.title ;
         });
     }
 
