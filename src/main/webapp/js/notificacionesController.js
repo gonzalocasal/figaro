e88e95324be59191404a3ca903067367caa453ff
gonzalo <gonzalocasal@gmail.com>
2018-05-19 17:25:20 -0300

    mejoras visuales

--- src/main/webapp/js/notificacionesController.js
+++ src/main/webapp/js/notificacionesController.js
@@ -10,7 +10,9 @@ app.controller('notificacionesController', function ($scope, $http) {
         $scope.getNotificaciones();
     }
 
+    //OBTENER NOTIFICACIONES
 	$scope.getNotificaciones = function(){
+		loading();
         $http.get("/rest/notificaciones",{params: {index: $scope.index}}).then(function (response) {
 			notificaciones = response.data;
         	Array.prototype.push.apply($scope.notificaciones, notificaciones);
@@ -18,25 +20,29 @@ app.controller('notificacionesController', function ($scope, $http) {
         });
     }
 
+    //MARCAR COMO LEIDAS TODAS LAS NOTIFICACIONES
     $scope.leer = function(){
         $http.put("/rest/notificaciones/leer").then(function (response) {
            $scope.cantidadNotificaciones=0;
+           loadComplete();
         });
     }
 
-
+    //OBTENER CANTIDAD DE NOTIFICACIONES SIN LEER
 	$scope.cantidad = function(){
         $http.get("/rest/notificaciones/cantidad").then(function (response) {
             $scope.cantidadNotificaciones = response.data;
         });
     }
 
+    //OBTENER CANTIDAD DE NOTIFICACIONES
     $scope.cantidadTotal = function(){
         $http.get("/rest/notificaciones/cantidad-total").then(function (response) {
             $scope.cantidadTotal = response.data;
         });
     }
 
+    //MAPER CATEGORIA DE NOTIFICACION CON ICONO
     $scope.getIcon=function(notifcacion){
     	return $scope.icons[notifcacion.categoria];
     }
@@ -45,7 +51,6 @@ app.controller('notificacionesController', function ($scope, $http) {
     $scope.verMas = function(){
         $scope.index ++;
         $scope.getNotificaciones();
-        
     }
 
 
